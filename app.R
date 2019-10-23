1dc5e4e9c7e2fc80ef0cde08477cbacb132f997d
Justin Millar <jjmillar@ufl.edu>
2019-10-23 21:00:00 +0100

    updating map and legends

--- app.R
+++ app.R
@@ -11,6 +11,7 @@ library(mgcv)
 library(splines)
 library(DT)
 library(gdistance)
+library(mapview)
 
 load('datafiles/gam_mod.rda')
 
@@ -25,13 +26,69 @@ T.filename <- 'study.area.T.rds'
 T.GC.filename <- 'study.area.T.GC.rds'
 fs1 <- raster("shp/friction-crop.tif")
 
-source("functions.R")
-
-hf_org <- read_csv("datafiles/hf-locations.csv")
+hf_org <- read_csv("datafiles/hf-locations.csv") 
 outline <- sf::st_read("shp/Study_Area_NEW.shp")
-grid <- sf::st_read("shp/grid_data_update.shp")
-sum_prev <- sum(grid$prv_pdt, na.rm = T)
-sum_case <- sum(grid$case_pdt, na.rm = T)
+grid <- sf::st_read("shp/grid_data_update.shp") %>% 
+  mutate(prv_pdt_org  = prv_pdt, 
+         cas_pdt_org = cas_pdt, 
+         prv_diff = prv_pdt - prv_pdt_org, 
+         cas_diff = cas_pdt - cas_pdt_org)
+
+# Functions
+update_prediction <- function(hfs) {
+  
+  points <- hfs %>% 
+    dplyr::select(x = Longitude, y = Latitude)
+  temp <- dim(points)
+  n.points <- temp[1]
+  
+  # Calculate time raster
+  if (transition.matrix.exists.flag == 1) {
+    T.GC <- readRDS(T.GC.filename)
+  } else {
+    t <- transition(fs1, function(x) 1/mean(x), 8) # RAM intensive, can be very slow for large areas
+    T.GC <- geoCorrection(t)
+  }
+  
+  # Convert the points into a matrix
+  xy.data.frame <- data.frame()
+  xy.data.frame[1:n.points,1] <- points[,1]
+  xy.data.frame[1:n.points,2] <- points[,2]
+  xy.matrix <- as.matrix(xy.data.frame)
+  
+  # Run the accumulated cost algorithm to make the final output map. This can be quite slow (potentially hours).
+  temp.raster <- accCost(T.GC, xy.matrix)
+  
+  grid_loc <- grid %>% 
+    as.data.frame() %>% 
+    dplyr::select(lng, lat) # Just 2 columns.  Structured as [X_COORD, Y_COORD] aka [LONG, LAT].  Use a header
+  
+  grid <- grid %>% 
+    mutate(time_hf  = extract(temp.raster, grid_loc), 
+           prev     = predict(gam_mod, grid, type = "response"), 
+           cases    = prev*grid$pop_den,
+           prv_pdt  = if_else(ck_full == 1, prev, NA_real_), 
+           cas_pdt  = if_else(ck_full == 1, cases, NA_real_), 
+           prv_diff = prv_pdt - prv_pdt_org, 
+           cas_diff = cas_pdt - cas_pdt_org)
+  
+  return(grid)
+}
+icon_color <- function(type) {
+  case_when(
+    type == "CHPS" ~ "red", 
+    type == "Health Center" ~ "blue", 
+    type == "User Defined" ~ "cadetblue")
+}
+get_domain <- function(x){
+  tmp <- max(abs(x))
+  out <- ifelse(is.na(tmp), 0.0001, tmp)
+  out <- ifelse(out == 0, 0.0001, out)
+  return(out)
+}
+
+sum_prev <- sum(grid$prv_pdt_org, na.rm = T)
+sum_case <- sum(grid$cas_pdt_org, na.rm = T)
 
 header <- dashboardHeader(
   title = "BYD Health Facilities"
@@ -52,7 +109,8 @@ body <- dashboardBody(
            box(width = NULL, status = "warning",
                title = "Add new health facility",
                p(class = "text-muted",
-                 "Click on the map to load a new Lat/Long for a propose location, and press the button below to refit the model"
+                 "Click on the map to load a new Lat/Long for a propose location, 
+                 and press the button below to refit the model."
                ),
                uiOutput("new_hf_text"),
                # actionButton("add_hf", "Add Facility"), 
@@ -76,18 +134,14 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output) {
-  
   vals <- reactiveValues()
   vals$hf_data <- hf_org
   vals$grid <- grid
   
-  output$hf_table=renderDataTable({vals$hf_data} )
-  
+  output$hf_table=renderDataTable({vals$hf_data})
   
   output$new_hf_text <- renderUI({
     
-    # click_dat <- unlist(input$prev_map_shape_click)
-    E
     lat <- unlist(input$prev_map_shape_click$lat)
     lon <- unlist(input$prev_map_shape_click$lng)
 
@@ -100,55 +154,94 @@ server <- function(input, output) {
 
  output$prev_map <- renderLeaflet({
   
-  prev_pal <- colorNumeric(palette = "inferno",na.color = "NA", domain = c(0,1))
-  case_pal <- colorNumeric(palette = "inferno", na.color = "NA",  domain = c(0, 200))
+  prev_pal <- colorNumeric(palette = "inferno", na.color = "#E1E1DD", domain = c(0,1))
+  case_pal <- colorNumeric(palette = "inferno", na.color = "#E1E1DD", domain = c(0, 200))
   
+  # Get max values for setting diverging color palette
+  # m_prev <- get_domain(vals$grid$prv_diff) + 0.001
+  # m_case <- get_domain(vals$grid$cas_diff) + 1
+  # m_prev <- max(abs(vals$grid$prv_diff))
+  # m_case <- max(abs(vals$grid$cas_diff))
+  
+  # prev_diff_pal <- colorNumeric(palette = "RdBu", na.color = "#E1E1DD", domain = c(-m_prev, m_prev))
+  # case_diff_pal <- colorNumeric(palette = "RdBu", na.color = "#E1E1DD", domain = c(-m_case, m_case))
+  prev_diff_pal <- colorNumeric(palette = "RdBu", na.color = "#E1E1DD", domain = c(-0.02, 0.02), reverse = T)
+  case_diff_pal <- colorNumeric(palette = "RdBu", na.color = "#E1E1DD", domain = c(-5, 5), reverse = T)
+  
+  icons <- awesomeIcons(icon = 'medkit', library = 'fa', iconColor = '#FFFFFF',
+                        markerColor = icon_color(vals$hf_data$Type))
+
   prev_map <- leaflet(vals$grid) %>%
     addPolygons(data = outline,
                 color = "black",
-                weight = 1,
-                opacity = 1, 
+                weight = 2,
+                opacity = 1,
                 fillOpacity = 0) %>%
     addPolygons(color = "NULL",
                 weight = 0, 
                 fillColor = ~prev_pal(prv_pdt), 
-                fillOpacity = 0.7, 
-                group = "Prevalence") %>%
+                fillOpacity = 1,
+                group = "Prevalence", 
+                label = sprintf("Prevalence: %.3f", vals$grid$prv_pdt) %>% 
+                  lapply(htmltools::HTML)) %>%
     addPolygons(color = "NULL",
                 weight = 0, 
                 fillColor = ~case_pal(cas_pdt), 
-                fillOpacity = 0.7, 
-                group = "Cases") %>%
+                fillOpacity = 1,
+                group = "Cases", 
+                label = sprintf("Exp. # of Cases: %.2f", vals$grid$cas_pdt) %>% 
+                  lapply(htmltools::HTML)) %>%
+    addPolygons(color = "NULL",
+                weight = 0, 
+                fillColor = ~prev_diff_pal(prv_diff), 
+                fillOpacity = 1,
+                group = "Diff. Prev", 
+                label = sprintf("Change in Prevalence: %.3f", vals$grid$prv_diff) %>% 
+                  lapply(htmltools::HTML)) %>%
+    addPolygons(color = "NULL",
+                weight = 0, 
+                fillColor = ~case_diff_pal(cas_diff), 
+                fillOpacity = 1,
+                group = "Diff. Cases", 
+                label = sprintf("Change in Exp. # of Cases: %.2f", vals$grid$cas_diff) %>% 
+                  lapply(htmltools::HTML)) %>%
     addAwesomeMarkers(data = vals$hf_data,
                       lng = vals$hf_data$Longitude,
                       lat = vals$hf_data$Latitude,
-                      popup = vals$hf_data$Name) %>%
-    addLegend(pal = prev_pal, values = ~prv_pdt,
+                      popup = vals$hf_data$Name, 
+                      icon = icons) %>%
+    addLegend(pal = prev_pal, values = ~prv_pdt, na.label = NULL, 
               title = "Prevalence:", 
-              group = "Prevalence") %>%
+              group = "Prevalence legend") %>%
     addLegend(pal = case_pal, values = ~cas_pdt,
               title = "Est. Cases:", 
-              group = "Cases") %>%
+              group = "Cases legend") %>%
+    addLegend(pal = prev_diff_pal, values = ~prv_diff,
+              title = "Change in Prevalence",
+              group = "Diff. Prev legend") %>%
+    addLegend(pal = case_diff_pal, values = ~cas_diff,
+              title = "Change in Exp. Cases",
+              group = "Diff. Cases legend") %>%
     addProviderTiles(providers$OpenStreetMap.BlackAndWhite) %>% 
+    addMiniMap(tiles = providers$OpenStreetMap.BlackAndWhite, 
+               position = "bottomleft",
+               toggleDisplay = TRUE, width = 120, height = 120) %>% 
+    mapview::addMouseCoordinates() %>%
     addLayersControl(
-      baseGroups = c("Prevalence", "Cases"), position = "bottomleft",
-      # overlayGroups = c("Prevalence", "Cases"),
-      options = layersControlOptions(collapsed = FALSE)
-    )
-  
+      baseGroups = c("Prevalence", "Cases", "Diff. Prev", "Diff. Cases"), 
+      overlayGroups = c("Prevalence legend", "Cases legend", "Diff. Prev legend", "Diff. Cases legend"),
+      position = "bottomleft",
+      options = layersControlOptions(collapsed = TRUE)
+    ) 
   prev_map
   })
  
  observeEvent(input$refit, {
-   
-   # click_dat <- unlist(input$prev_map_shape_click)
-   
    lat <- unlist(input$prev_map_shape_click$lat)
    lon <- unlist(input$prev_map_shape_click$lng)
    
    vals$hf_data <- vals$hf_data %>% 
      add_case(
-       # Name = paste("New Facility", row_number(.) - 8),
        Name = "New Facility",
        Latitude = round(lat, 3), 
        Longitude = round(lon, 3),
@@ -161,8 +254,6 @@ server <- function(input, output) {
    vals$hf_data <- hf_org
    vals$grid <- grid
  })  
- 
- 
 }
  
 shinyApp(ui, server)
--- functions.R
+++ functions.R
@@ -1,5 +1,3 @@
-
-
 load('datafiles/gam_mod.rda')
 
 area.shape <- readOGR("shp/Study_Area_NEW.shp")
@@ -13,15 +11,9 @@ bottom <- 50.0
 top    <- 52.0
 transition.matrix.exists.flag <- 0 # if the geo-corrected graph has already been made, this can save time.  Uses the same T.GC.filename as specified using the T.GC.filename variable.
 
-# Input Files
-# friction.surface.filename <- 'shp/friction_surface/friction_surface_2015_v1.0.tif'
-# point.filename <- 'data/hf-locations.csv' # Just 2 columns.  Structured as [X_COORD, Y_COORD] aka [LONG, LAT].  Use a header.
-
 # Output Files
 T.filename <- 'study.area.T.rds'
 T.GC.filename <- 'study.area.T.GC.rds'
-# friction <- raster("output/shp/friction-crop.tif")
-# fs1 <- crop(friction, extent(area.shape))
 fs1 <- raster("shp/friction-crop.tif")
 
 # Write functions ----
@@ -34,14 +26,10 @@ update_prediction <- function(hfs) {
   
   # Calculate time raster
   if (transition.matrix.exists.flag == 1) {
-    # Read in the transition matrix object if it has been pre-computed
     T.GC <- readRDS(T.GC.filename)
   } else {
-    # Make and geocorrect the transition matrix (i.e., the graph)
     t <- transition(fs1, function(x) 1/mean(x), 8) # RAM intensive, can be very slow for large areas
-    # saveRDS(T, T.filename)
     T.GC <- geoCorrection(t)
-    # saveRDS(T.GC, T.GC.filename)
   }
   
   # Convert the points into a matrix
@@ -52,38 +40,19 @@ update_prediction <- function(hfs) {
   
   # Run the accumulated cost algorithm to make the final output map. This can be quite slow (potentially hours).
   temp.raster <- accCost(T.GC, xy.matrix)
-  
-  # # Update travel time 
-  # grid_loc <- dplyr::select(grid, lng, lat) # Just 2 columns.  Structured as [X_COORD, Y_COORD] aka [LONG, LAT].  Use a header
-  # grid$time_hf <- extract(temp.raster, grid_loc)
-  
-  # Make updated prediction 
-  # pred <- predict(gam_mod, grid_dat, type = "response")
-  
  
     grid_loc <- grid %>% 
       as.data.frame() %>% 
       dplyr::select(lng, lat) # Just 2 columns.  Structured as [X_COORD, Y_COORD] aka [LONG, LAT].  Use a header
-    # 
-    # time_hf = extract(temp.raster, grid_loc)
-    # prev = predict(gam_mod, grid, type = "response")
-    # cases = prev*grid$pop_den
-    # if_else(grid$ck_full == 1, prev, NA_real_)
-    
+
     
     grid <- grid %>% 
       mutate(time_hf = extract(temp.raster, grid_loc), 
-             prev = predict(gam_mod, grid, type = "response"), 
-             cases = prev*grid$pop_den,
+             prev    = predict(gam_mod, grid, type = "response"), 
+             cases   = prev*grid$pop_den,
              prv_pdt = if_else(ck_full == 1, prev, NA_real_), 
              cas_pdt = if_else(ck_full == 1, cases, NA_real_))
     
-    # grid <- grid %>% 
-    #   mutate(prv_pdt = if_else(ck_full == "TRUE", prev, NA_real_), 
-    #          cas_pdt = if_else(ck_full == "TRUE", cases, NA_real_))
-    
     return(grid)
-    
-    
-  
+
 }
